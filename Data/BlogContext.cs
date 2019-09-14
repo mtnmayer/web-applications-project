207bf700466a539d42bcfa000ace059998cbe8db
eyal666 <eyal.roman@gmail.com>
2019-09-14 16:57:27 +0300

    Add models

---
+++ Data/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelMe.Models;
+
+namespace TravelMe.Data
+{
+    public class BlogContext: DbContext
+    {
+        public BlogContext(DbContextOptions<BlogContext> options): base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories{ get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Place> Places { get; set; }
+        public DbSet<PlaceCategory> PlaceCategories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelMe.Models
+{
+    public class Comment
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        [Required]
+        public int PostID { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public User User { get; set; }
+        [Required]
+        public Post Post { get; set; }
+
+    }
+}
